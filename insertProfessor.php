614066ba2da68e537fcdc4b07000a492b1b9eecc
Peter Lin <peterlin741@gmail.com>
2013-05-07 03:53:54 -0400

    Updated insertProfessor.php

--- insertProfessor.php
+++ insertProfessor.php
@@ -17,18 +17,47 @@ $mylastname = $_POST['lastName'];
 $mysecurityquestion = $_POST['securityQuestion'];
 $mysecurityanswer = $_POST['securityAnswer'];
 
-mysql_query("INSERT INTO Professor (netid, password, firstName, lastName, securityQuestion, 
-securityAnswer)
-VALUES
-('$mynetid', '$mypassword', '$myfirstname', '$mylastname', '$mysecurityquestion', 
-'$mysecurityanswer')");
+//condition to add professor
+$addprofessor = 1;
 
+//make sure each form is filled out
+foreach($_POST as $key => $value) {
+	if((!isSet($value)) or (!$value) or ($value = "")) {
+		$addprofessor = 0;
+	}
+}
+
+//echo message if forms aren't filled out
+if($addprofessor == 0){
+	echo "Please fill out all forms. ";
+}
+
+//make sure there does not exist a addprofessor with the same netID
+$query = mysql_query("SELECT * FROM Professor WHERE netid = '$mynetid'");
+$samenetid = mysql_num_rows($query);
+
+//if there are any rows, there is already a professor with the same netid
+if($samenetid > 0) {
+		$addprofessor = 0;
+		echo "An account with that netID already exists. ";
+}
+
+//add professor into the database
+if($addprofessor == 1){
+	mysql_query("INSERT INTO Professor (netid, password, firstName, lastName, securityQuestion, 
+	securityAnswer)
+	VALUES
+	('$mynetid', '$mypassword', '$myfirstname', '$mylastname', '$mysecurityquestion', 
+	'$mysecurityanswer')");
+	
+	echo "Account added. <br>";
+}
 
 
 $redirectionTime = 3;
 $newPageUrl = "index.php";
 header( "Refresh: $redirectionTime; url=$newPageUrl" );
-echo "Account added. You will now be redirected to the home page, after $redirectionTime seconds.";
+echo "You will now be redirected to the home page, after $redirectionTime seconds.";
 
 
 
